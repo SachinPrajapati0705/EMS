315e11afdebf14f4e9f17020e5aa50bd65aaee9f
SachinPrajapati0705 <121787300+SachinPrajapati0705@users.noreply.github.com>
2023-10-06 11:44:13 +0530

    Update Event.java

--- Event.java
+++ Event.java
@@ -11,4 +11,42 @@ public class Event {
     private String description;
 
     // getters and setters
+    @Column(columnDefinition = "TEXT") // Use TEXT for longer descriptions
+    private String description;
+    
+    // Additional parameters
+    private String organizer;
+    private int maxParticipants;
+    private boolean registrationOpen;
+
+    // Constructors (if needed)
+    
+    // Getters and Setters for additional parameters
+    public String getOrganizer() {
+        return organizer;
+    }
+
+    public void setOrganizer(String organizer) {
+        this.organizer = organizer;
+    }
+
+    public int getMaxParticipants() {
+        return maxParticipants;
+    }
+
+    public void setMaxParticipants(int maxParticipants) {
+        this.maxParticipants = maxParticipants;
+    }
+
+    public boolean isRegistrationOpen() {
+        return registrationOpen;
+    }
+
+    public void setRegistrationOpen(boolean registrationOpen) {
+        this.registrationOpen = registrationOpen;
+    }
+
+    // Getters and Setters for existing parameters (name, date, location, description)
+
+    // Other methods if needed
 }
